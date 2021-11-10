091b3eee39c139a8a0edd312cbefe943b14af267
andrey.piekas <andrey.piekas@CHEINFD03.alunos.sc.senac.br>
2021-11-09 21:27:39 -0300

    Inicio do repositório externo.

--- src/br/com/nextlevel/view/TelaClientesEnderecos.java
+++ src/br/com/nextlevel/view/TelaClientesEnderecos.java
@@ -34,13 +34,10 @@ public class TelaClientesEnderecos extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "CEP", "RUA", "NUMERO", "COMPLEMENTO", "BAIRRO", "CIDADE", "ESTADO"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -56,9 +53,9 @@ public class TelaClientesEnderecos extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 545, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(55, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 496, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
